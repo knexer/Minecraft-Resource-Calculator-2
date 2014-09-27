6190c75aa431fee1217ac24b6b68406354086a99
UntouchedWagons <jordan.w.bradley@gmail.com>
2014-09-26 21:10:09 -0400

    Moved API stuff to its own repository

--- src/main/java/untouchedwagons/minecraft/mcrc2/api/recipes/exceptions/InvalidRecipeException.java
+++
@@ -1,8 +0,0 @@
-package untouchedwagons.minecraft.mcrc2.api.recipes.exceptions;
-
-public class InvalidRecipeException extends Exception {
-    public InvalidRecipeException(String message)
-    {
-        super(message);
-    }
-}
--- src/main/java/untouchedwagons/minecraft/mcrc2/api/recipes/exceptions/UnknownRecipeException.java
+++
@@ -1,9 +0,0 @@
-package untouchedwagons.minecraft.mcrc2.api.recipes.exceptions;
-
-public class UnknownRecipeException extends Exception
-{
-    public UnknownRecipeException(String message)
-    {
-        super(message);
-    }
-}
